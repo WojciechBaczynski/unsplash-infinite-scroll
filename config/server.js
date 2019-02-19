600d3be9dbd3af698a468d055535b9fb744d17fd
Wojciech Baczyński <wp.baczynski@gmail.com>
2019-02-19 13:10:44 +0100

    Add app sheleton

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'c1180a54cc5bc3de4ddff9a4c1a5ab979c81f548fc53d1e63d08e65f0dec1484',
+    SECRET: process.env.SECRET || '9317bee3a7eaeb897b18aaa63d3e306e9d2d89956d702e19e676dec54511bd54',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
